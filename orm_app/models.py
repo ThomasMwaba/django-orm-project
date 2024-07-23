ab1e1c7beb62a94239fc7eff01e27d9d82754903
mwangelwa <mwangelwa@hotmail.com>
2024-07-23 14:02:43 +0300

    foreign keys added

--- orm_app/models.py
+++ orm_app/models.py
@@ -1,70 +1,92 @@
-from django.db import models
 import uuid
 
-
+from django.db import models
 
 # Create your models here.
 
+
+class Catergory(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    is_active = models.BooleanField(default=False)
+    parent = models.ForeignKey("self", on_delete=models.PROTECT)
+
+
+class SeasonalEvents(models.Model):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    name = models.CharField(max_length=100, unique=True)
+
+
+class ProductType(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey("self", on_delete=models.CASCADE)
+
+
 class Product(models.Model):
-    
     IN_STOCK = "IS"
     OUT_OF_STOCK = "OOS"
     BACKORDERED = "BO"
-    
+
     STOCK_STATUS = {
         IN_STOCK: "In Stock",
         OUT_OF_STOCK: "Out Of Stock",
         BACKORDERED: "Back Ordered",
-        }
-    
+    }
+
     pid = models.CharField(max_length=255)
-    name = models.CharField(max_length=100)
-    slug = models.SlugField()
-    description = models.TextField()
-    is_digital = models.BooleanField()
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(null=True)
+    is_digital = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(auto_now=True,editable=False)
-    is_active = models.BooleanField()
-    status = models.CharField(max_length=3,choices=STOCK_STATUS,default=OUT_OF_STOCK)
-    
-    
-    
-    
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    is_active = models.BooleanField(default=False)
+    status = models.CharField(max_length=3, choices=STOCK_STATUS, default=OUT_OF_STOCK)
+    catergory = models.ForeignKey(Catergory, on_delete=models.SET_NULL, null=True)
+    seasonal_event = models.ForeignKey(
+        SeasonalEvents, on_delete=models.SET_NULL, null=True
+    )
+    product_type = models.ManyToManyField(ProductType, related_name="product_type")
+
+
+class Attribute(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True)
+
+
+class AttributeValue(models.Model):
+    attributevalue = models.CharField(max_length=100)
+    attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
+
+
 class ProductLine(models.Model):
-    
-    price = models.DecimalField()
+    price = models.DecimalField(decimal_places=2, max_digits=5)
     sku = models.UUIDField(default=uuid.uuid4)
-    slug = models.SlugField()
-    stock_qty = models.IntegerChoices()
-    is_active = models.BooleanField()
+    slug = models.SlugField(unique=True)
+    stock_qty = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=False)
     order = models.IntegerField()
     weight = models.FloatField()
-    
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    attribute_values = models.ManyToManyField(
+        AttributeValue, related_name="attribute_values"
+    )
+
+
 class ProductImage(models.Model):
-    
-      name = models.CharField(max_length=100)
-      alternative_text = models.CharField(max_length=100)
-      url = models.ImageField()
-      order = models.IntegerField()
-      
-      
-      
-class Catergory(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.SlugField()
-    is_active = models.BooleanField()
-    
-    
-class SeasonalEvents(models.Model):
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    name = models.CharField(max_length=100)
-    
-    
-      
-      
+    alternative_text = models.CharField(max_length=100)
+    url = models.ImageField()
+    order = models.IntegerField()
+    product_line = models.ForeignKey(ProductLine, on_delete=models.CASCADE)
 
-      
-    
-    
 
+class ProductLine_AttributeValue(models.Model):
+    attribute_value = models.ForeignKey(AttributeValue, on_delete=models.CASCADE)
+    product_line = models.ForeignKey(ProductLine, on_delete=models.CASCADE)
+
+
+class Product_ProductType(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
